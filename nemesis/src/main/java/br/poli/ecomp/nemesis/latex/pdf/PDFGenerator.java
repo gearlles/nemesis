4cdbdf8b9aca2ac2990c1e32b0d20cb01428a050
gearlles <gearlles@gmail.com>
2013-12-11 02:06:38 -0300

    PDF generator

---
+++ nemesis/src/main/java/br/poli/ecomp/nemesis/latex/pdf/PDFGenerator.java
@@ -0,0 +1,33 @@
+package br.poli.ecomp.nemesis.latex.pdf;
+
+import java.io.File;
+import java.io.IOException;
+
+import de.nixosoft.jlr.JLRGenerator;
+
+public class PDFGenerator {
+
+	private File pdfLatexFile = new File(
+			"C:\\Program Files (x86)\\MiKTeX 2.9\\miktex\\bin\\pdflatex.exe");
+	private File texFile = new File(
+			"C:\\Users\\gearl_000\\Desktop\\tutorial\\test.tex");
+	private File outputFolder = new File(
+			"C:\\Users\\gearl_000\\Desktop\\tutorial");
+
+	public boolean generate() {
+        try {
+            JLRGenerator pdfGen = new JLRGenerator();  
+            
+			if (!pdfGen.generate(pdfLatexFile, texFile, outputFolder, outputFolder)) { 
+			    System.out.println(pdfGen.getErrorMessage());
+			} else {
+				pdfGen.deleteTempFiles(false, true, true);
+			}
+			System.out.println(pdfGen.getOutputStream());
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+}
